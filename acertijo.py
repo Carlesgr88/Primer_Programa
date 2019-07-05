c65a3fcea73f02b3053659b525b0a7c413507e59
carlesgr88 <uraduna14>
2019-07-05 17:54:49 +0200

    Añadiendo diferentes pruebas

---
+++ acertijo.py
@@ -0,0 +1,28 @@
+
+
+numero_adivinar=8
+numero_usuario=int(input("dime un numero del 1 al 10: "))
+if numero_usuario == numero_adivinar:
+    print("has acertado")
+else:
+    numero_usuario=int(input("No has acertado, dime otro numero: "))
+
+if numero_usuario== numero_adivinar:
+    print("has acertado")
+else:
+    numero_usuario=int(input("No has acertado, dime otro numero: "))
+
+if numero_usuario== numero_adivinar:
+    print("has acertado")
+else:
+    numero_usuario=int(input("No has acertado, dime otro numero: "))
+
+if numero_usuario == numero_adivinar:
+    print("has acertado")
+else:
+    numero_usuario=int(input("No has acertado, dime otro numero: "))
+if numero_usuario == numero_adivinar:
+    print("has acertado")
+else:
+    print("te has equivocado, el numero a adivinar era {}".format(numero_adivinar))
+
---
+++ test_if_else.py
@@ -0,0 +1,6 @@
+"Programam de prueba if/else"
+numero_del_usuario=int(input("dime un numero: "))
+if numero_del_usuario > 100:
+    print("enhorabuena tu numero es mayor que 100")
+else:
+    print("Ps no es el 100 lokillo")
